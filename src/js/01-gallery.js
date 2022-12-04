eaec5fb4d2023642f3c421d448e4529e86fdbe76
IvanHolovakha <meisuiwanreiser@gmail.com>
2022-12-04 19:25:00 +0200

    changes in 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Change code below this line
+
 console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+
+createGaleryMarkup(galleryItems);
+
+function createGaleryMarkup (galleryItems) {
+    const markup = galleryItems.map(({preview, original, description}) => {
+        return `
+            <a class="gallery__item" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+                />
+            </a>
+        `;
+    }
+).join("");
+    galleryRef.insertAdjacentHTML("beforeend", markup);
+};
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+})
