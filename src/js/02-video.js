eaf6b7f74e2a51ee5ee9294f77893263cfb1b606
IvanHolovakha <meisuiwanreiser@gmail.com>
2022-12-05 01:19:39 +0200

    add 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player'
+import throttle from 'lodash.throttle'
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+playerCurrentTime();
+
+function playerCurrentTime(){
+    const currentTime = localStorage.getItem("videoplayer-current-time");
+    if(currentTime) {
+        player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+    };
+};
+
+player.on('timeupdate', throttle(({seconds})=>{
+    localStorage.setItem("videoplayer-current-time", seconds)
+    }, 1000));
