06e1e2fcafbf3361e23562f6f6222e675b29aba8
IvanHolovakha <meisuiwanreiser@gmail.com>
2022-12-06 01:04:26 +0200

    add 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from "lodash.throttle";
 
+const formRef = document.querySelector(".feedback-form");
+
+const STORAGE_KEY = "feedback-form-state";
+const formData = {};
+
+populateForm();
+
+formRef.addEventListener("input", throttle(onInput, 500));
+formRef.addEventListener("submit", onSubmit);
+
+function onInput (e) {
+    formData[e.target.name]=e.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData)); 
+};
+
+function onSubmit (e) {
+    e.preventDefault();
+    localStorage.removeItem(STORAGE_KEY);
+    e.currentTarget.reset();
+    console.log(formData);
+};
+
+function populateForm(){
+    const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    const email = document.querySelector('input');
+    const message = document.querySelector('textarea');
+    if (savedData) {
+        email.value = savedData.email;
+        message.value = savedData.message;
+    };
+};
